d7b40463897d3485db2d6f9bfd8d789aed953c12
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-15 12:13:15 -0300

    test: add validator and ensure return false if validator returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,22 @@
+import { EmailValidatorAdapter } from './email-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
+
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
+describe('EmailValidator Adapter', () => {
+  // se a biblioteca retornar false o método tbm retorna false
+  test('Should return false if validator returns false', () => {
+    const sut = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+})
