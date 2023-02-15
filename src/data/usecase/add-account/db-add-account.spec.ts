0f89d94ab52b42e2efdfa0a0f0fff3c810bbd7aa
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-15 16:22:20 -0300

    test: ensure return throw if Encrypter throws

--- src/data/usecase/add-account/db-add-account.spec.ts
+++ src/data/usecase/add-account/db-add-account.spec.ts
@@ -88,4 +88,17 @@ describe('DbAccount Usecase', () => {
       password: 'any_password'
     })
   })
+
+  // teste para garantir q se houver alguma excessão será repassada..
+  test('Should throw if Encrypter throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
