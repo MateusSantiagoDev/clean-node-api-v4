bc41c13f1d8a94aa104153c1fc05c5632569bb1c
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-15 17:37:43 -0300

    test: ensure call bcrypt with correct values

---
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,23 @@
+import { BcryptAdapter } from './bcrypt-adapter'
+import bcrypt from 'bcrypt'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hash'))
+  }
+}))
+
+const salt = 12
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
+describe('Bcrypt Adapter', () => {
+  // teste para garantir a integração do adapter com a biblioteca
+  test('Should call bcrypt with correct values', async () => {
+    const sut = makeSut()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toBeCalledWith('any_value')
+  })
+})
