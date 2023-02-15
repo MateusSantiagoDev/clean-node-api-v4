77050ca76134da41f8c3e78247acd4e9b1525e24
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-15 00:25:51 -0300

    feat: ensure return 400 if no email id provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -3,9 +3,17 @@ import { MissingParamError } from '../../error/missing-param-error'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    return {
-      statusCode: 400,
-      body: new MissingParamError('name')
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError('name')
+      }
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError('email')
+      }
     }
   }
 }
