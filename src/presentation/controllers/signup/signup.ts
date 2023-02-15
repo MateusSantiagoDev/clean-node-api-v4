0d0319ffebb38f0b41b82c011db76e5c7be0f7df
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-15 00:19:18 -0300

    feat: add missingParamError method

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,10 +1,11 @@
 import { HttpRequest, HttpResponse } from '../../protocols/http'
+import { MissingParamError } from '../../error/missing-param-error'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     return {
       statusCode: 400,
-      body: new Error('missing param error')
+      body: new MissingParamError('name')
     }
   }
 }
