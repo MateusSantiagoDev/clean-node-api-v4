000c643e3ac41484279259a820910026b0f31d0e
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-15 10:56:23 -0300

    test: ensure return 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -5,6 +5,7 @@ import { EmailValidator } from '../../protocols/email-validator'
 import { ServerError } from '../../error/server-error'
 import { AddAccount, AddAccountModel } from '../../../data/domain/usecase/add-account'
 import { AccountModel } from '../../../data/domain/model/account'
+import { ok } from '../../helpers/http-helper'
 
 const makeAddAccounStub = (): AddAccount => {
   class AddAccountStub implements AddAccount {
@@ -213,5 +214,26 @@ describe('SignUp Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  // retornar status 200 em caso de sucesso
+  test('Should return 200 if valid data is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
+        confirmPassword: 'valid_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    })
   })
 })
