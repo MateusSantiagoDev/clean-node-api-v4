db13161bca577da7ef1f89c98eb381fb881a8424
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-15 23:22:15 -0300

    refactor: group middleware interface in a single file

--- src/main/config/middlewars.ts
+++ src/main/config/middlewars.ts
@@ -1,7 +1,5 @@
 import { Express } from 'express'
-import { bodyParser } from '../middlewares/body-parser'
-import { cors } from '../middlewares/cors'
-import { contentType } from '../middlewares/content-type'
+import { bodyParser, cors, contentType } from '../middlewares'
 
 // aqui será configuradas as rotas
 export default (app: Express): void => {
